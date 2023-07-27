d4070c2dc841986fcdc512b1e6c44780fa2eb092
Esteban Rebuffé-Mareau <estebanianis@gmail.com>
2023-07-27 19:11:53 +0200

    Clean the code

--- src/main/java/chatop/apiRest/Auth/AuthController.java
+++ src/main/java/chatop/apiRest/Auth/AuthController.java
@@ -3,7 +3,6 @@ package chatop.apiRest.Auth;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +24,4 @@ public class AuthController {
     public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) {
         return ResponseEntity.ok(authService.register(request));
     }
-
 }
