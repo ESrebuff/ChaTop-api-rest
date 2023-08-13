ee03c3a0566bd340845a9467f4b6b31479d0dcdc
Esteban Rebuffé-Mareau <estebanianis@gmail.com>
2023-08-13 18:06:45 +0200

    Delete the useless imports

--- src/main/java/chatop/apiRest/service/RentalServiceImpl.java
+++ src/main/java/chatop/apiRest/service/RentalServiceImpl.java
@@ -9,8 +9,6 @@ import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
 import org.springframework.web.multipart.MultipartFile;
